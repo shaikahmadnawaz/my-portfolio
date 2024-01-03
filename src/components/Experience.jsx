aeb5c47439171407a293ac8f95da535515837dd1
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-01-03 17:46:36 +0530

    updated experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,39 +1,72 @@
-import React from "react";
-
 function Experience() {
   const experiences = [
     {
-      title: "Web Development Intern",
+      title: "Full Stack Development Intern",
       company: "Atoms Digital Solutions",
-      duration: "March 2023 - Present",
-      description:
-        "Experienced MERN stack web developer specializing in efficient and scalable web applications. Passionate about delivering high-quality code and creating seamless user experiences. Collaborating with a talented team at Atoms Group to bring innovative solutions to life. Let's turn your ideas into reality with cutting-edge technology.",
-    },
-    {
-      title: "Open Source Contributor",
-      company: "GirlScript Summer of Code",
-      duration: "May 2023 - Present",
-      description:
-        "Passionate open source contributor actively participating in GirlScript Summer of Code. Contributing to meaningful projects and collaborating with a diverse community to solve real-world challenges. Dedicated to enhancing coding skills, fostering innovation, and making a positive impact in the tech industry.",
+      logo: "./atoms.jpg",
+      duration: "Mar 2023 - Sep 2023",
+      description: `Played a key role in developing three comprehensive websites for the Atoms Group and its affiliated wings.
+      Utilized MERN stack and AWS technologies. Successfully established a digital presence, contributing to a 20% growth in online visibility.
+      Implemented features that facilitated the acquisition of new clients, resulting in a 15% increase in client engagement.`,
     },
 
-    // Add more experiences here
+    {
+      title: "Open Source Contributor",
+      company: "Time Chain Labs, Node.js, Dart, BioDrop, and more",
+      logo: "./open-source.webp",
+      duration: "Apr 2023 - Present",
+      description: `Contributed to Bitcoin Token Standards project as part of Timechain Labs.
+      Contributed to Dart docs.
+      Contributed to Node.js website, BioDrop, and SeamlessUI projects.`,
+    },
   ];
 
   return (
     <div id="#experience" className="relative w-full py-10 bg-white">
       <div className="mx-auto max-w-7xl px-8">
         <h1 className="text-3xl font-bold mb-4 md:text-center">Experience</h1>
-        <p className="text-lg text-gray-600 mb-8 md:text-center">
+        <p className="text-lg text-black mb-8 md:text-center">
           Check out my professional experience below.
         </p>
-        <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 grid-cols-1 lg:grid-cols-2">
           {experiences.map((experience, index) => (
-            <div key={index} className="bg-gray-100 p-6 rounded-lg">
-              <h2 className="text-xl font-semibold mb-2">{experience.title}</h2>
-              <h3 className="text-gray-600 mb-2">{experience.company}</h3>
-              <p className="text-gray-600 mb-4">{experience.duration}</p>
-              <p className="text-gray-800">{experience.description}</p>
+            <div
+              key={index}
+              className="flex max-w-2xl flex-col items-center rounded-md border md:flex-row shadow-sm"
+            >
+              <div className="h-full w-full md:h-[200px] md:w-[400px] p-2">
+                <img
+                  src={experience.logo}
+                  alt="company logo"
+                  className="h-full w-full rounded-md object-cover"
+                />
+              </div>
+              <div>
+                <div className="p-4">
+                  <h2 className="inline-flex items-center text-lg font-semibold">
+                    {experience.title}
+                  </h2>
+                  <div className="mt-3 flex items-center space-x-2">
+                    <span className="flex flex-col">
+                      <span className="text-[10px] font-medium text-black">
+                        {experience.company}
+                      </span>
+                      <span className="text-[8px] font-medium text-black">
+                        {experience.duration}
+                      </span>
+                    </span>
+                  </div>
+                  <div className="mt-3 text-sm text-black text-justify">
+                    <ul className="list-disc pl-5">
+                      {experience.description
+                        .split("\n")
+                        .map((point, index) => (
+                          <li key={index}>{point}</li>
+                        ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
