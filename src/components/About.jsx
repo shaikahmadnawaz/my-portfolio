7aaa7d7c7501b9931343d7ccab666b5fc4d23ca0
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-01-03 14:11:32 +0530

    updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="#about" className="bg-white py-10">
-      <div className="max-w-7xl mx-auto px-8">
+    <div id="#about" className="bg-white py-2 md:py-10">
+      <div className="max-w-7xl mx-auto">
         <div className="lg:flex lg:items-center lg:gap-x-8">
-          <div className="w-full lg:w-1/2">
+          <div className="w-full lg:w-1/2 hidden md:block">
             <img
               className="w-full h-auto object-cover rounded-lg"
               src="about-me.svg"
@@ -15,23 +15,28 @@ const About = () => {
           <div className="mt-8 lg:mt-0 lg:w-1/2">
             <div className="px-4 py-8 md:py-10 lg:py-12">
               <h2 className="text-3xl font-bold mb-4">About Me</h2>
-              <p className="text-lg mb-4">
-                Hi, I'm Nawaz, a Full Stack Web Developer with a focus on MERN
-                stack (MongoDB, Express.js, React.js, and Node.js). I have a
-                passion for creating web applications that provide seamless user
-                experiences and solve real-world problems.
+              <p className="text-lg mb-4 text-justify">
+                Greetings! I&apos;m Nawaz, a results-driven Full Stack Web
+                Developer specializing in the dynamic MERN stack (MongoDB,
+                Express.js, React.js, and Node.js). My journey involves a deep
+                passion for crafting web applications that not only deliver
+                seamless user experiences but also address real-world
+                challenges.
               </p>
-              <p className="text-lg mb-4">
-                I have built numerous web applications, ranging from e-commerce
-                platforms to social networking sites. My expertise lies in
-                designing and developing scalable and efficient solutions that
-                adhere to industry best practices.
+              <p className="text-lg mb-4 text-justify">
+                My portfolio boasts a diverse range of web applications,
+                spanning from innovative e-commerce platforms to engaging social
+                networking sites. I pride myself on creating scalable and
+                efficient solutions that adhere to industry best practices,
+                ensuring optimal performance and user satisfaction.
               </p>
-              <p className="text-lg">
-                I'm excited to take on new challenges and contribute to
-                innovative projects. Feel free to connect with me through the
-                social links below or reach out via email. Let's build something
-                amazing together!
+              <p className="text-lg text-justify">
+                Eager to tackle new challenges, I am committed to contributing
+                my skills to innovative projects. Whether it&apos;s enhancing
+                user interactions, optimizing performance, or creating intuitive
+                designs, I&apos;m up for the task. Let&apos;s connect through
+                the social links below or drop me an email. Together, let&apos;s
+                embark on the journey of building something extraordinary!
               </p>
             </div>
           </div>
